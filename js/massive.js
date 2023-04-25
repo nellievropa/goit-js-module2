dd8eaf84b1dc1186be3de21291a9050a56afce66
nellievropa <nellievropa23@gmail.com>
2023-04-25 16:12:41 +0300

    початок

---
+++ js/massive.js
@@ -0,0 +1,57 @@
+
+// массиви
+// const friends = ["Mango", "Kiwi", "Poly", "Ajax"];
+
+// console.table(friends);
+
+// console.log(friends[1]);
+
+
+// const lastIndex = friends.length - 1;
+// console.log(lastIndex);
+
+// friends[2] = 'fdtrug';
+// console.log(friends);
+
+// примитиві і складні типи
+
+// let a = 10;
+// let b = a;
+
+// console.log(a);
+// console.log(b);
+
+// a =20;
+
+// console.log(a);
+// console.log(b);
+
+
+// складні типи
+
+// const a = [1, 2, 3, 4]
+// const b = a;
+
+// console.log('a', a)
+// console.log('b', b);
+
+// a[1] = 500;
+
+// console.log('a', a)
+// console.log('b', b);
+
+// console.log(a === b);
+
+// // массиви не равні, потому что у них разніе ячейки для хранения информации
+
+// console.log([1, 2, 2] === [1, 2, 2]);
+
+
+// перебор массивов FOR
+
+const friends = ["Mango", "Kiwi", "Poly", "Ajax"];
+
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
