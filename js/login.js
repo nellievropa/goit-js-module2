36d0f782d291831bf9a3f99a973120b33c1b3798
nellievropa <nellievropa23@gmail.com>
2023-04-26 14:53:18 +0300

    login !

--- js/login.js
+++ js/login.js
@@ -5,26 +5,42 @@
 
 const logins = ['dfdretefd', 'mnkjiuuyjh', 'qwqeqewrwre', 'popolkkjhiuj', 'zxzxzsaxz'];
 const loginToFind = 'popolkkjhiuj';
-let message = '';
+let message = `User ${loginToFind} is not finded !!!!`;
 
-for(let i = 0; i < logins.length; i +=1 ) {
-    const login = logins[i];
+// for(let i = 0; i < logins.length; i +=1 ) {
+//     const login = logins[i];
 
-//     if (login !== loginToFind) {
-//         message = `User ${loginToFind} is not finded `;
-//     } else {
-//         message = `User ${loginToFind} is finded !!!!`;
-//     }
-// //    не работает, потому что цикл не прерівается и он перебирает все до конца 
+//     console.log(`Login: `, login);
+
+// //     if (login !== loginToFind) {
+// //         message = `User ${loginToFind} is not finded `;
+// //     } else {
+// //         message = `User ${loginToFind} is finded !!!!`;
+// //     }
+// // //    не работает, потому что цикл не прерівается и он перебирает все до конца 
+// // }
+// // console.log(message);
+
+// // нужно делать наоборот
+// if (login === loginToFind) {
+//     message = `User ${loginToFind} is finded `;
+//     break;
+// }   
+
 // }
 // console.log(message);
 
-// нужно делать наоборот
-if (login === loginToFind) {
-    message = `User ${loginToFind} is finded `;
-    break;
-} 
-    message = `User ${loginToFind} is not finded !!!!`;
+// можно записать через FOR OF
 
+for (const login of logins) {
+    console.log(`Login: `, login);
+    console.log(`${login} === ${loginToFind}: `, login === loginToFind);
+
+    if (login === loginToFind) {
+        console.log('Ура равни!!!');
+        message = `User ${loginToFind} is finded `;
+        break;
+    }   
+    
 }
 console.log(message);
